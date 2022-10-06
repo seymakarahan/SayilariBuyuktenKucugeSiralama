034dc4cd0612bb03de4da28c7d7a2660dd8daf30
seymakarahan <85848981+seymakarahan@users.noreply.github.com>
2022-10-06 11:00:16 +0300

    Create SayilariBuyuktenKucugeSiralama.java

---
+++ SayilariBuyuktenKucugeSiralama.java
@@ -0,0 +1,39 @@
+package SayilariBuyuktenKucugeSiralama;
+
+import java.util.Scanner;
+
+public class SayilariBuyuktenKucugeSiralama {
+    public static void main(String[] args) {
+        int a, b, c;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Birinci sayı : ");
+        a = input.nextInt();
+
+        System.out.print("İkinci sayı : ");
+        b = input.nextInt();
+
+        System.out.print("Üçüncü sayı : ");
+        c = input.nextInt();
+
+        if ((a > b) && (a > c)) {
+            if (b > c) {
+                System.out.println("a > b > c");
+            } else {
+                System.out.println("a > c > b");
+            }
+        } else if ((b > a) && (b > c)) {
+            if (a > c) {
+                System.out.println("b > a > c");
+            } else {
+                System.out.println("b > c > a");
+            }
+        } else if ((c > a) && (c > b)) {
+            if (a > b) {
+                System.out.println("c > a > b");
+            } else {
+                System.out.println("c > b > a");
+            }
+        }
+    }
+}
